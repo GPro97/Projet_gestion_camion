0ff7bb0c3e2457518dc10d3393694bb41faf8396
Geoffrey Kraus <geoffrey.kraus77@gmail.com>
2019-03-13 10:11:35 +0100

    De 0 à 170 degree fonctionne aussi

--- Code_arduino/Code_arduino.ino
+++ Code_arduino/Code_arduino.ino
@@ -44,7 +44,7 @@ void loop() {
 //  delay(2000);
   servo_barriere.write(90);
   delay(2000);
-  servo_barriere.write(165);
+  servo_barriere.write(170);
   delay(2000);
   servo_barriere.write(0);
   delay(2000);
