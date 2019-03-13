55b380242a7b4147564b1725f64448ca709028f5
Geoffrey Kraus <geoffrey.kraus77@gmail.com>
2019-03-13 10:10:07 +0100

    Code servo de 0 à 165 degree sans controle de vitesse

---
+++ Code_arduino/Code_arduino.ino
@@ -0,0 +1,52 @@
+
+#include <Servo.h>
+
+#define LEDPIN 13
+#define Etat_barriere 4
+
+//Variable relatif à la barrière
+Servo servo_barriere;
+
+void setup() {
+  noInterrupts(); // désactiver toutes les interruptions
+
+  pinMode(LEDPIN, OUTPUT); // ledpin juste pour voir si ca fonctionne.
+  pinMode(Etat_barriere, INPUT);
+  digitalWrite(LEDPIN, HIGH);
+
+  servo_barriere.attach(5); //Attache le servomoteur de la barriere au pin 5
+  pinMode(Etat_barriere, INPUT); //Définir Etat_barriere en entrée
+
+  interrupts(); //Activer toutes les interruptions
+
+  //Définir l'état initial du servo
+  servo_barriere.write(0);
+
+  delay(10000); //Attente de 2s avant de démarrer réellement le code
+
+}
+
+
+void barriere(void){
+  servo_barriere.write(90);
+}
+
+void loop() {
+//  servo_barriere.write(30);
+//  delay(2000);
+//  servo_barriere.write(60);
+//  delay(2000);
+//  servo_barriere.write(90);
+//  delay(2000);
+//  servo_barriere.write(120);
+//  delay(2000);
+//  servo_barriere.write(150);
+//  delay(2000);
+  servo_barriere.write(90);
+  delay(2000);
+  servo_barriere.write(165);
+  delay(2000);
+  servo_barriere.write(0);
+  delay(2000);
+
+}
