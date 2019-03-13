3148f5a9af32c06a9c119dfa590116a1765abe54
Geoffrey Kraus <geoffrey.kraus77@gmail.com>
2019-03-13 10:52:19 +0100

    Fonctionnement du servo avec BP

--- Code_arduino/Code_arduino.ino
+++ Code_arduino/Code_arduino.ino
@@ -2,7 +2,8 @@
 #include <Servo.h>
 
 #define LEDPIN 13
-#define Etat_barriere 4
+int Etat_barriere;
+int pin_interrupt = 4;
 
 //Variable relatif à la barrière
 Servo servo_barriere;
@@ -11,42 +12,32 @@ void setup() {
   noInterrupts(); // désactiver toutes les interruptions
 
   pinMode(LEDPIN, OUTPUT); // ledpin juste pour voir si ca fonctionne.
-  pinMode(Etat_barriere, INPUT);
+  pinMode(pin_interrupt, INPUT);//Définir la pin de l'interrupteur en entrée
   digitalWrite(LEDPIN, HIGH);
 
   servo_barriere.attach(5); //Attache le servomoteur de la barriere au pin 5
-  pinMode(Etat_barriere, INPUT); //Définir Etat_barriere en entrée
 
   interrupts(); //Activer toutes les interruptions
 
   //Définir l'état initial du servo
   servo_barriere.write(0);
-
+  
   delay(10000); //Attente de 2s avant de démarrer réellement le code
 
 }
 
 
-void barriere(void){
-  servo_barriere.write(90);
-}
+//void barriere(void){
+//  servo_barriere.write(90);
+//}
 
 void loop() {
-//  servo_barriere.write(30);
-//  delay(2000);
-//  servo_barriere.write(60);
-//  delay(2000);
-//  servo_barriere.write(90);
-//  delay(2000);
-//  servo_barriere.write(120);
-//  delay(2000);
-//  servo_barriere.write(150);
-//  delay(2000);
-  servo_barriere.write(90);
-  delay(2000);
-  servo_barriere.write(170);
-  delay(2000);
-  servo_barriere.write(0);
-  delay(2000);
+  Etat_barriere = digitalRead(pin_interrupt);
+  if (Etat_barriere == HIGH){
+    servo_barriere.write(90);
+  }
+  else{
+    servo_barriere.write(10);
+  } 
 
 }
