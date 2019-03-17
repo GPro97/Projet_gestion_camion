7e34d97998179af0c67323123aa60552f7b7035a
Geoffrey Kraus <geoffrey.kraus77@gmail.com>
2019-03-17 15:26:32 +0100

    Contrôle de vitesse du servo-moteur fonctionnant

--- Code_arduino/Code_arduino.ino
+++ Code_arduino/Code_arduino.ino
@@ -3,41 +3,82 @@
 
 #define LEDPIN 13
 int Etat_barriere;
-int pin_interrupt = 4;
+int pin_interrupt = 2;
 
-//Variable relatif à la barrière
+const int temps_barriere_entre_2degrees = 15;
+const int temps_barriere_attente = 2000;
+long t_actuel;
+int position_barriere = 0;
+bool flag_barriere_montee = false;
+bool flag_barriere_descente = false;
+bool flag_barriere_attente = false;
+bool flag_maintien_barriere = false;
+
+//Variable relatif Ã  la barriÃ¨re
 Servo servo_barriere;
 
 void setup() {
-  noInterrupts(); // désactiver toutes les interruptions
+  noInterrupts(); // dÃ©sactiver toutes les interruptions
 
   pinMode(LEDPIN, OUTPUT); // ledpin juste pour voir si ca fonctionne.
-  pinMode(pin_interrupt, INPUT);//Définir la pin de l'interrupteur en entrée
-  digitalWrite(LEDPIN, HIGH);
+  pinMode(pin_interrupt, INPUT);//DÃ©finir la pin de l'interrupteur en entrÃ©e
 
-  servo_barriere.attach(5); //Attache le servomoteur de la barriere au pin 5
+  servo_barriere.attach(3); //Attache le servomoteur de la barriere au pin 5
 
   interrupts(); //Activer toutes les interruptions
 
-  //Définir l'état initial du servo
+  //DÃ©finir l'Ã©tat initial du servo
   servo_barriere.write(0);
+  Serial.begin(9600);
   
-  delay(10000); //Attente de 2s avant de démarrer réellement le code
+  delay(2000); //Attente de 2s avant de dÃ©marrer rÃ©ellement le code
 
 }
 
 
-//void barriere(void){
-//  servo_barriere.write(90);
-//}
+void barriere(void){
+  if(!flag_barriere_montee && !flag_barriere_attente && !flag_barriere_descente){
+    if (millis() - t_actuel >= temps_barriere_entre_2degrees) {
+      position_barriere ++ ;
+      servo_barriere.write(position_barriere);
+      t_actuel = millis();
+      if (position_barriere == 90){
+        flag_barriere_montee = true;
+      }
+    }
+  }
+
+  if(flag_barriere_montee && !flag_barriere_attente && !flag_barriere_descente){
+    if(millis() - t_actuel >= temps_barriere_attente){
+      flag_barriere_attente = true;
+    }
+  }
+
+  if(flag_barriere_montee && flag_barriere_attente && !flag_barriere_descente){
+    if (millis() - t_actuel >= temps_barriere_entre_2degrees) {
+      position_barriere -- ;
+      servo_barriere.write(position_barriere);
+      t_actuel = millis();
+      if (position_barriere == 0){
+        flag_barriere_descente = true;
+      }
+    }
+  }
+
+  if(flag_barriere_montee && flag_barriere_attente && flag_barriere_descente){
+    flag_barriere_montee = false;
+    flag_barriere_attente = false;
+    flag_barriere_descente = false;
+    flag_maintien_barriere = false;
+  }
+}
 
 void loop() {
+  digitalWrite(LEDPIN, HIGH);
   Etat_barriere = digitalRead(pin_interrupt);
-  if (Etat_barriere == HIGH){
-    servo_barriere.write(90);
+  if (Etat_barriere == HIGH | flag_maintien_barriere == HIGH){
+    flag_maintien_barriere = true;
+    barriere();
   }
-  else{
-    servo_barriere.write(10);
-  } 
-
 }
+
