d8857ce0a83d339b5205faf842f40c83925568b1
Geoffrey Kraus <geoffrey.kraus77@gmail.com>
2019-05-10 17:48:54 +0200

    Ajout de commentaires - Non fini

--- Code_arduino/Code_arduino.ino
+++ Code_arduino/Code_arduino.ino
@@ -1,8 +1,9 @@
-
+//This library are used for the lcd screen and the motor
 #include <Servo.h>
 #include <LiquidCrystal.h>
 
 #define LEDPIN 13
+
 int Etat_barriere;
 int pin_interrupt = 2;
 int led_rouge = 6;
@@ -21,31 +22,26 @@ bool flag_barriere_attente = false;
 bool flag_maintien_barriere = false;
 bool etat_led = false;
 
-//Variable relatif Ã  la barriÃ¨re
+//Variable relatif à la barrière
 Servo servo_barriere;
 
 LiquidCrystal lcd(7,8,9,10,11,12);
 
 void setup() {
-  noInterrupts(); // dÃ©sactiver toutes les interruptions
 
-  pinMode(LEDPIN, OUTPUT); // ledpin juste pour voir si ca fonctionne.
-  pinMode(pin_interrupt, INPUT);//DÃ©finir la pin de l'interrupteur en entrÃ©e
-  pinMode(led_rouge, OUTPUT);
-  pinMode(potentiometre, INPUT);
+  //Then we can assign each pin to input or output.
+                                  //Reminder:
+  pinMode(LEDPIN, OUTPUT);        //Pin 13
+  pinMode(pin_interrupt, INPUT);  //Pin 2
+  pinMode(led_rouge, OUTPUT);     //Pin 6
+  pinMode(potentiometre, INPUT);  //Pin A0
 
-  servo_barriere.attach(3); //Attache le servomoteur de la barriere au pin 5
-
-  interrupts(); //Activer toutes les interruptions
-
-  //DÃ©finir l'Ã©tat initial du servo
-  servo_barriere.write(89);
-  Serial.begin(9600);
-
-  lcd.begin(16,2);
+ 
+  servo_barriere.attach(3);//The servo-motor has to be attach to a pin. Here the pin 3
+  servo_barriere.write(89);//Because of the way the servo motor is put, we have to tilt the barrier to 89° so that it is straight
+  lcd.begin(16,2);//This is a configuration of the screen's size
   
-  delay(2000); //Attente de 2s avant de dÃ©marrer rÃ©ellement le code
-
+  delay(2000); //When the setup is done, we wait 2 seconds. A lot of forums and website give this recommendation.
 }
 
 
@@ -105,17 +101,18 @@ void clignotement(float temps_clignotement){
 }
 
 void loop() {
-  digitalWrite(LEDPIN, HIGH);
-  pot_binaire = analogRead(potentiometre);
-  float frequence=map(pot_binaire,0,1005,1,6);
-  tempo = (1/frequence) * 1000;
+  digitalWrite(LEDPIN, HIGH);                     //When the program is looping, the small led is alight.
+
+  pot_binaire = analogRead(potentiometre);        //The value of the analog potentiometer is recovered.
+  float frequence = map(pot_binaire,0,1005,1,6);  //This instruction gives a value include between 1 and 6. It's the frequency of the red LED.
+  tempo = (1/frequence) * 1000;                   //The frequency is converted to seconds.
   
-  Etat_barriere = digitalRead(pin_interrupt);
-  
+  Etat_barriere = digitalRead(pin_interrupt);     //The value of the input pin is read
+
+  //Here, we check two condition. Firstly, if the raspberry detect a number plate. And secondly, if the gate is closed. If only one is HIGH, the condition is confirm.
   if (Etat_barriere == HIGH | flag_maintien_barriere == HIGH){
-    flag_maintien_barriere = true;
+    flag_maintien_barriere = true;//This flag authorise to pass if the gate is in movement
     barriere();
     clignotement(tempo);
   }
 }
-
